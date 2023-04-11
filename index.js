579040d7f95f56664cc263442cba435908ebb75c
Douglas Gonçalves <60992128+douglas-ggoncalves@users.noreply.github.com>
2023-04-10 22:33:47 -0300

    Update index.js

--- index.js
+++ index.js
@@ -42,10 +42,10 @@ app.get("/pagar", async (req, res) => {
     }
 })
 
-app.post("/not", (req, res)){
+app.post("/not", (req, res) => {
     console.log(req.query);
     res.send("OK")
-}
+})
 
 app.listen(8080, (req, res) => {
     console.log("Servidor rodando")
